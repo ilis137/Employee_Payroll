c83c4f92ad9671f775f1ed086705b9341a76651c
ilis137 <iliyasshahapure1@gmail.com>
2022-10-21 20:22:54 +0530

    add:read from file and print data to console

--- src/main/java/com/javapractice/EmployeePayrollService.java
+++ src/main/java/com/javapractice/EmployeePayrollService.java
@@ -1,49 +1,50 @@
 package com.javapractice;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
     private List<Payroll> employeePayrollList;
-    public enum IOService {CONSOLE_IO, FILE_IO };
+
+    public enum IOService {
+        CONSOLE_IO, FILE_IO
+    };
+
     public EmployeePayrollService() {
         this.employeePayrollList = new ArrayList<Payroll>();
     }
 
-  
+    public EmployeePayrollService(List payrolls) {
+        this.employeePayrollList = payrolls;
+    }
+    private void readEmployeePayroll(Scanner consoleINputReader) {
+        System.out.println("Enter Employee ID: ");
+        int id = consoleINputReader.nextInt();
+        System.out.println("Enter Employee Name: ");
+        String name = consoleINputReader.next();
+        System.out.println("Enter Employee Salary: ");
+        double salary = consoleINputReader.nextDouble();
+        employeePayrollList.add(new Payroll(id, name, salary));
+    }
 
-  private void readEmployeePayroll(Scanner consoleINputReader) {
-      System.out.println("Enter Employee ID: ");
-      int id = consoleINputReader.nextInt();
-      System.out.println("Enter Employee Name: ");
-      String name = consoleINputReader.next();
-      System.out.println("Enter Employee Salary: ");
-      double salary = consoleINputReader.nextDouble();
-      employeePayrollList.add(new Payroll(id,name,salary));
-  }
+    public void writeEmployeePayroll(IOService ioService) {
+        if (ioService.equals(IOService.CONSOLE_IO))
+            employeePayrollList.stream().forEach(System.out::println);
+        else if (ioService.equals(IOService.FILE_IO))
+            new EmployeePayrollFileIOService().write(employeePayrollList);
+    }
 
-    
-  public void writeEmployeePayroll(IOService ioService) {
-    if(ioService.equals(IOService.CONSOLE_IO))
-        employeePayrollList.stream().forEach(System.out::println);
-    else if (ioService.equals(IOService.FILE_IO))
-        new EmployeePayrollFileIOService().write(employeePayrollList);
-}
+    public void printData(IOService ioService) {
+        if (ioService.equals(IOService.FILE_IO))
+            new EmployeePayrollFileIOService().printData();
+    }
 
-
-
-public void printData(IOService ioService) {
-    if (ioService.equals(IOService.FILE_IO))
-        new EmployeePayrollFileIOService().printData();
-}
-
-
-
-public long countEntries(IOService ioService) {
-    if (ioService.equals(IOService.FILE_IO))
-        return new EmployeePayrollFileIOService().countEntries();
-    return 0;
-}
+    public long countEntries(IOService ioService) {
+        if (ioService.equals(IOService.FILE_IO))
+            return new EmployeePayrollFileIOService().countEntries();
+        return 0;
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Payroll Service program");
@@ -52,7 +53,7 @@ public long countEntries(IOService ioService) {
         employeePayrollService.readEmployeePayroll(consoleINputReader);
         employeePayrollService.writeEmployeePayroll(IOService.FILE_IO);
         employeePayrollService.countEntries(IOService.FILE_IO);
+        employeePayrollService.printData(IOService.FILE_IO);
     }
 
-  
 }
--- src/test/java/com/javapractice/NIOFileAPITest.java
+++ src/test/java/com/javapractice/NIOFileAPITest.java
@@ -1,6 +1,5 @@
 package com.javapractice;
 
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,66 +8,71 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class NIOFileAPITest {
     private static String HOME = System.getProperty("user.home");
     private static String PLAY_WITH_NIO = "TempPlayGround";
-    Path playPath = Paths.get(HOME + "/"+PLAY_WITH_NIO);
+    Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
     Path homePath = Paths.get(HOME);
-    
-     //Check File Exists
+
+    // Check File Exists
     @Test
     public void checkFileExists() throws IOException {
         Assert.assertTrue(Files.exists(homePath));
     }
-    
-     //Delete File And Check File Not Exist
+
+    // Delete File And Check File Not Exist
     @Test
-    public void checkFileNotExists(){
-         if (Files.exists(playPath)) FileUtils.deleteFiles(playPath.toFile());
-         Assert.assertTrue(Files.notExists(playPath));
- 
-    }    
-     //Create Directory
-    @Test
-    public void checkCreateDirectory() throws IOException{
-    
-     Files.createDirectories(playPath);
-     Assert.assertTrue(Files.exists(playPath));
+    public void checkFileNotExists() {
+        if (Files.exists(playPath))
+            FileUtils.deleteFiles(playPath.toFile());
+        Assert.assertTrue(Files.notExists(playPath));
 
     }
-     //Create File
+
+    // Create Directory
     @Test
-    public void checkCreateFile(){
-          IntStream.range(1,10).forEach(fileNumber ->{
-            Path tempFile = Paths.get(playPath + "/temp"+fileNumber);
+    public void checkCreateDirectory() throws IOException {
+
+        Files.createDirectories(playPath);
+        Assert.assertTrue(Files.exists(playPath));
+
+    }
+
+    // Create File
+    @Test
+    public void checkCreateFile() {
+        IntStream.range(1, 10).forEach(fileNumber -> {
+            Path tempFile = Paths.get(playPath + "/temp" + fileNumber);
             Assert.assertTrue(Files.notExists(tempFile));
             try {
                 Files.createFile(tempFile);
-            }catch (IOException e){}
+            } catch (IOException e) {
+            }
             Assert.assertTrue(Files.exists(tempFile));
-        } );
+        });
 
     }
 
-    //List Files, Directories as well as Files with Extension
+    // List Files, Directories as well as Files with Extension
     @Test
-    public void checkListFiles() throws IOException{
+    public void checkListFiles() throws IOException {
         Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
         Files.newDirectoryStream(playPath).forEach(System.out::println);
-        Files.newDirectoryStream(playPath , path -> path.toFile().isFile() && path.toString().startsWith("temp"))
+        Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
                 .forEach(System.out::println);
     }
 
-
-    //watch files
+    // watch files
     @Test
     public void givenADirectoryWhenWatchedListsAllTheActivities() throws IOException {
         Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
         Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
         new Java8WatchService(dir).processEvents();
-        Path newFile=Paths.get(HOME+"/"+PLAY_WITH_NIO+"/test");
+        Path newFile = Paths.get(HOME + "/" + PLAY_WITH_NIO + "/test");
         Assert.assertTrue(Files.exists(newFile));
     }
+
 }
